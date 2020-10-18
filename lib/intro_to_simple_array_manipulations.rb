3a3005fb93462e727e5f8c1c146ca6cd0003f86b
Justin Movitz <movitzj1@montclair.edu>
2020-10-18 17:34:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,20 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def pop_with_args(array)
+  last_2_array_items = array.pop(2)
+  return last_2_array_items
+end
+
+def using_shift(array)
+  new_lst = array.shift
+  return new_lst
+end
+
+def shift_with_args(array)
+  new_lst_2 = array.shift(2)
+  return new_lst_2
+end
